614a5c4ab4f04e8d10ee7690b284d30539693a5f
dchakov <dchakov@yahoo.com>
2015-09-27 10:28:34 +0300

    Documentation.chm

--- BalloonsPopGame/src/Player.cs
+++ BalloonsPopGame/src/Player.cs
@@ -1,12 +1,19 @@
 namespace BalloonsPopGame.Srs
 {
     using System;
-
+    /// <summary>
+    /// Class containing player info
+    /// </summary>
+   
     public class Player : IComparable<Player>
     {
         private int value;
         private string name;
-
+        /// <summary>
+        /// Constructor for palyer
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="name"></param>
         public Player(int value, string name)
         {
             this.Value = value;
@@ -24,7 +31,11 @@ namespace BalloonsPopGame.Srs
             get { return this.name; }
             set { this.name = value; }
         }
-
+        /// <summary>
+        /// Method for compare two players
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public int CompareTo(Player other)
         {
             return this.Value.CompareTo(other.Value);
